1bc377199241d00b8e984544dd467011b1b3cb74
Eric Fulmer <eric.r.fulmer@gmail.com>
2018-09-17 21:55:59 -0400

    ENH: first (trivial) test

---
+++ src/tests/test_models.py
@@ -0,0 +1,8 @@
+"""Unit tests for the database models."""
+# from ..models import Book
+
+
+class TestBooks:
+    """Tests for the Book model."""
+    def test_inserting_duplicate_isbn_fails(self):
+        pass
